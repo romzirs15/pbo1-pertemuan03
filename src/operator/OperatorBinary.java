7b2a7f50b57ae6eb7b04503f2386f1c3df7cafc2
romzirokan <romzirs15@gmail.com>
2021-04-24 21:24:59 +0800

    Operator Binary Assingment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //Operator (=) sebagai assigment/penugasan
+    //Menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Romzi Rokan Suri";
+    int jumlahsks = 24;
+    double indeksPrestasiKumulatif = 3.52;
+
+
+}
