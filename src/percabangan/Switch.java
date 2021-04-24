d1bb65c1a5ab691448056a254a042880c595f0d2
romzirokan <romzirs15@gmail.com>
2021-04-24 22:13:45 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int JalanJalan=3;
+        switch (JalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            case 5:
+                System.out.println("makhluk halus");
+                break;
+        }
+    }
+}
