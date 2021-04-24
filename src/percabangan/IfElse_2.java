53bfdf40f8a9349fa879435037d77ba7671c3129
romzirokan <romzirs15@gmail.com>
2021-04-24 21:56:09 +0800

    Percabangan IfElse Condition False

---
+++ src/percabangan/IfElse_2.java
@@ -0,0 +1,15 @@
+package percabangan;
+
+public class IfElse_2 {
+    public static void main(String[] args) {
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
+
+        if(uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi belanja anda");
+        }else{
+            System.out.println("Uang cukup, selamat menikmati");
+        }
+    }
+
+}
